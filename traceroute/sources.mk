fd48ab1f081ce386a3642c47bbd60257d4960bcb
Moutig <moutig@moutig.sh>
2026-02-25 14:48:10 +0000

    Build(traceroute): Add Makefile, sources.mk, update gitignore and hajlib submodule
    - Added Makefile for ft_traceroute with build rules, hajlib integration, and colorized output
    - Added sources.mk defining source files, object files, and build directories
    - Updated .gitignore to include ft_traceroute binary
    - Updated hajlib submodule to latest commit

---
+++ traceroute/sources.mk
@@ -0,0 +1,19 @@
+include ../common/sources.mk
+
+SRC_DIR		= src
+BUILD_DIR	= build
+HAJ_DIR		= src
+
+COMMON_DIR		= ../common
+COMMON_BUILD	= $(COMMON_DIR)/build
+
+# Sources
+SRC			= $(SRC_DIR)/main.c \
+			  $(SRC_DIR)/parser.c \
+			  $(SRC_DIR)/usage.c
+
+HAJ_SRC		= 
+
+# Objects
+OBJ			= $(addprefix $(BUILD_DIR)/, $(notdir $(SRC:.c=.o)))
+HAJ_OBJ		= $(addprefix $(BUILD_DIR)/haj/, $(notdir $(SRC:.c=.o)))
